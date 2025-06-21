876e968f6031d575e72a5c37f0e71a63a515a349
WIBUloli11 <quanglammaivan@gmail.com>
2025-06-21 10:43:44 +0700

    sua tinh nang slug

--- server/save.php
+++ server/save.php
@@ -23,13 +23,13 @@ function slugify($str)
     $str = strtolower($str);
 
     $unicode = array(
-        'a' => 'á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ',
-        'd' => 'đ',
-        'e' => 'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
-        'i' => 'í|ì|ỉ|ĩ|ị',
-        'o' => 'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
-        'u' => 'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
-        'y' => 'ý|ỳ|ỷ|ỹ|ỵ',
+        'a' => 'á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ|Á|À|Ả|Ã|Ạ|Ă|Ắ|Ằ|Ẳ|Ẵ|Ặ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+        'd' => 'đ|Đ',
+        'e' => 'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ|É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+        'i' => 'í|ì|ỉ|ĩ|ị|Í|Ì|Ỉ|Ĩ|Ị',
+        'o' => 'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ|Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+        'u' => 'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự|Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+        'y' => 'ý|ỳ|ỷ|ỹ|ỵ|Ý|Ỳ|Ỷ|Ỹ|Ỵ'
     );
     foreach ($unicode as $nonUnicode => $uni) {
         $str = preg_replace("/($uni)/i", $nonUnicode, $str);
@@ -82,7 +82,7 @@ foreach ($content['blocks'] as $block) {
 }
 
 // Lấy danh sách file trong thư mục uploads (bao gồm cả uploads/files/)
-$existingFiles = array_diff(scandir($uploadDir. 'images/'), ['.', '..']);
+$existingFiles = array_diff(scandir($uploadDir . 'images/'), ['.', '..']);
 $existingAttachFiles = array_diff(scandir($uploadDir . 'files/'), ['.', '..']);
 $targetNumber = $user['user_id'] ?? 'default'; // Số cần lọc (user_id)
 
