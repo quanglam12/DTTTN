1f401e876974945631c051b78f007c3b6c52195e
WIBUloli11 <quanglammaivan@gmail.com>
2025-06-22 20:36:27 +0700

    change settings.php

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php
 
-$Domain = "http://localhost/";
+//$Domain = "http://localhost/";
 //$Domain = "http://146.196.64.79/";
-//$Domain = "https://doantnttn.com/";
+$Domain = "https://doantnttn.com/";
